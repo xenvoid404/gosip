a38373955b89395d7f13e41268e8e6a9741da5f3
xenvoid404 <xenvoid404@gmail.com>
2026-09-10 06:10:56 +0800

    add constant http status dan fitur futur yang palibg sering dugunakan untuk membangun api.

--- context.go
+++ context.go
@@ -1,12 +1,63 @@
 package gosip
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 type Context struct {
 	ResponseWriter http.ResponseWriter
 	Request        *http.Request
+	handlers       []HandlerFunc
+	index          int
+	statusCode     int
+	wroteHeader    bool
+	errors         []error
+}
+
+func (c *Context) Next() {
+	c.index++
+	if c.index < len(c.handlers) {
+		c.handlers[c.index](c)
+	}
+}
+
+func (c *Context) Status(code int) *Context {
+	c.statusCode = code
+	return c
+}
+
+func (c *Context) JSON(data any) *Context {
+	c.ResponseWriter.Header().Set("Content-Type", "application/json")
+	c.writeHeader()
+	if err := json.NewEncoder(c.ResponseWriter).Encode(data); err != nil {
+		c.Error(err)
+	}
+	return c
+}
+
+func (c *Context) String(data string) *Context {
+	c.ResponseWriter.Header().Set("Content-Type", "text/plain")
+	c.writeHeader()
+	if _, err := c.ResponseWriter.Write([]byte(data)); err != nil {
+		c.Error(err)
+	}
+	return c
+}
+
+func (c *Context) Error(err error) {
+	c.errors = append(c.errors, err)
+}
+
+func (c *Context) writeHeader() {
+	if c.wroteHeader {
+		return
+	}
+	if c.statusCode == 0 {
+		c.statusCode = StatusOK
+	}
+	c.ResponseWriter.WriteHeader(c.statusCode)
+	c.wroteHeader = true
 }
 
 func (c *Context) Query(key string) string {
--- router.go
+++ router.go
@@ -1,16 +1,192 @@
 package gosip
 
 import (
+	"context"
+	"errors"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+)
+
+const (
+	defaultReadHeaderTimeout = 5 * time.Second
+	defaultReadTimeout       = 15 * time.Second
+	defaultWriteTimeout      = 15 * time.Second
+	defaultIdleTimeout       = 60 * time.Second
+	defaultShutdownTimeout   = 10 * time.Second
 )
 
 type HandlerFunc func(*Context)
+type ErrorHandlerFunc func([]error, *Context)
 
 type Router struct {
-	mux         *http.ServeMux
-	middlewares []HandlerFunc
+	mux          *http.ServeMux
+	middlewares  []HandlerFunc
+	prefix       string
+	root         *Router
+	notFound     HandlerFunc
+	errorHandler ErrorHandlerFunc
 }
 
 func New() *Router {
-	return &Router{mux: http.NewServeMux()}
+	r := &Router{
+		mux: http.NewServeMux(),
+	}
+	r.root = r
+	r.notFound = defaultNotFound
+	r.errorHandler = defaultErrorHandler
+	r.mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		c := &Context{
+			ResponseWriter: w,
+			Request:        req,
+			handlers:       []HandlerFunc{r.root.notFound},
+			index:          -1,
+		}
+		c.Next()
+		r.handleErrors(c)
+	})
+
+	return r
+}
+
+func (r *Router) Use(middleware ...HandlerFunc) {
+	r.middlewares = append(r.middlewares, middleware...)
+}
+
+func (r *Router) Group(prefix string) *Router {
+	return &Router{
+		mux:          r.mux,
+		middlewares:  append([]HandlerFunc{}, r.middlewares...),
+		prefix:       joinPaths(r.prefix, prefix),
+		root:         r.root,
+		notFound:     r.root.notFound,
+		errorHandler: r.root.errorHandler,
+	}
+}
+
+func (r *Router) Get(pattern string, handlers ...HandlerFunc) {
+	r.handle(http.MethodGet, pattern, handlers...)
+}
+
+func (r *Router) Post(pattern string, handlers ...HandlerFunc) {
+	r.handle(http.MethodPost, pattern, handlers...)
+}
+
+func (r *Router) Put(pattern string, handlers ...HandlerFunc) {
+	r.handle(http.MethodPut, pattern, handlers...)
+}
+
+func (r *Router) Patch(pattern string, handlers ...HandlerFunc) {
+	r.handle(http.MethodPatch, pattern, handlers...)
+}
+
+func (r *Router) Delete(pattern string, handlers ...HandlerFunc) {
+	r.handle(http.MethodDelete, pattern, handlers...)
+}
+
+func (r *Router) Options(pattern string, handlers ...HandlerFunc) {
+	r.handle(http.MethodOptions, pattern, handlers...)
+}
+
+func (r *Router) Head(pattern string, handlers ...HandlerFunc) {
+	r.handle(http.MethodHead, pattern, handlers...)
+}
+
+func (r *Router) handle(method, pattern string, handlers ...HandlerFunc) {
+	fullPath := joinPaths(r.prefix, pattern)
+	fullPattern := method + " " + fullPath
+
+	all := make([]HandlerFunc, 0, len(r.middlewares)+len(handlers))
+	all = append(all, r.middlewares...)
+	all = append(all, handlers...)
+
+	r.mux.HandleFunc(fullPattern, func(w http.ResponseWriter, req *http.Request) {
+		c := &Context{
+			ResponseWriter: w,
+			Request:        req,
+			handlers:       all,
+			index:          -1,
+		}
+		c.Next()
+		r.handleErrors(c)
+	})
+}
+
+func (r *Router) handleErrors(c *Context) {
+	if len(c.errors) == 0 {
+		return
+	}
+	if !c.wroteHeader {
+		r.root.errorHandler(c.errors, c)
+		return
+	}
+	for _, err := range c.errors {
+		log.Printf("gosip: error setelah response dikirim: %v", err)
+	}
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	r.mux.ServeHTTP(w, req)
+}
+
+func (r *Router) Listen(addr string) error {
+	srv := &http.Server{
+		Addr:              addr,
+		Handler:           r,
+		ReadHeaderTimeout: defaultReadHeaderTimeout,
+		ReadTimeout:       defaultReadTimeout,
+		WriteTimeout:      defaultWriteTimeout,
+		IdleTimeout:       defaultIdleTimeout,
+	}
+	return r.ListenWithServer(srv)
+}
+
+func (r *Router) ListenWithServer(srv *http.Server) error {
+	if srv.Handler == nil {
+		srv.Handler = r
+	}
+
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- srv.ListenAndServe()
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(quit)
+
+	select {
+	case err := <-errCh:
+		if errors.Is(err, http.ErrServerClosed) {
+			return nil
+		}
+		return err
+	case sig := <-quit:
+		log.Printf("menerima signal %s, mematikan server...", sig)
+		ctx, cancel := context.WithTimeout(context.Background(), defaultShutdownTimeout)
+		defer cancel()
+		return srv.Shutdown(ctx)
+	}
+}
+
+func joinPaths(prefix, path string) string {
+	if prefix == "" {
+		return path
+	}
+	if path == "" {
+		return prefix
+	}
+	return strings.TrimRight(prefix, "/") + "/" + strings.TrimLeft(path, "/")
+}
+
+func defaultNotFound(c *Context) {
+	c.Status(StatusNotFound).String("404 Not Found")
+}
+
+func defaultErrorHandler(errs []error, c *Context) {
+	c.Status(StatusInternalServerError).String(errs[0].Error())
 }
